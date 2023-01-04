21fa56aa58aad1aadd3d0de1734b8aa1466d4c03
yuanchingchen <yuanching.chen.appledev@gmail.com>
2023-01-04 15:59:20 -0800

    [UPDATE] Add Google credentials

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // import syntax with commonJS module
-
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // rounte handler
-
+passport.use(new GoogleStrategy());
 
 var server = app.listen(PORT, function() {
     console.log('Listening on port %d', server.address().port);
